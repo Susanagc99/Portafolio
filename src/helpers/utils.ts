88a7e2fb3f41e545cdae9978d6b41a6436f554d0
Susanagc <susigc0304@gmail.com>
2025-10-22 10:46:24 -0500

    counter

---
+++ src/helpers/utils.ts
@@ -0,0 +1,44 @@
+import { toast } from "react-toastify";
+
+export const notifications = (text: string, type: "success"|"error"|"warn") => {
+    if (type === "success") {
+        toast.success(text, {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
+
+    if (type === "error") {
+        toast.error(text, {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
+
+    if (type === "warn") {
+        toast.warn(text, {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
+
+
+};
